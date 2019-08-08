1bfb9d80c11c3a3501586175b1c74209d7abec31
TuanAnh <tanhit243@gmail.com>
2019-08-08 13:32:35 +0700

    update mysql:MYSQL_ALLOW_EMPTY_PASSWORD

---
+++ docker/mysql/docker-entrypoint-initdb.d/create_user_demo.sql
@@ -0,0 +1,10 @@
+
+# If there are any .sh or .sql scripts you want to run on the database immediately after it has been created, 
+# you can put them into a host directory and then mount the directory at /docker-entrypoint-initdb.d/ inside the container.
+# For example, for a MySQL Server container https://dev.mysql.com/doc/mysql-installation-excerpt/8.0/en/docker-mysql-more-topics.html
+
+# file .sh, .sql will be auto run when conatainer be created and started 
+CREATE USER 'caeruxlab'@'localhost' IDENTIFIED BY '.caerux';
+CREATE DATABASE IF NOT EXISTS `db_demo_1` ;
+GRANT ALL ON `db_demo_1`.* TO 'caeruxlab'@'localhost' ;
+FLUSH PRIVILEGES;
